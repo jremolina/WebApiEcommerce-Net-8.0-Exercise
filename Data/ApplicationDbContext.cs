a2c92c2e7862375059b2fb3bbfa39b78fcb5a435
Jhonny Remolina <asistenteinde@redsis.com>
2026-02-18 18:24:47 -0500

    Creacion de Proyecto y Primera Migracion

---
+++ Data/ApplicationDbContext.cs
@@ -0,0 +1,15 @@
+using System;
+using ApiEcommerce.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace ApiEcommerce.Data;
+
+public class ApplicationDbContext : DbContext
+{
+    public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+    {
+    }
+
+    public DbSet<Category> Categories { get; set; }
+    
+}
---
+++ Models/Category.cs
@@ -0,0 +1,14 @@
+using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace ApiEcommerce.Models;
+
+public class Category
+{
+    [Key]
+    public int Id { get; set; }
+    [Required]
+    public string Name { get; set; } = string.Empty;
+    [Required]
+    public DateTime CreationDate { get; set; }
+}
